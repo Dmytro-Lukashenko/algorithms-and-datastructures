ead4b40680c61b3fb887d9a03276a97113b3c5ed
Dmytro Lukashenko <ext.dmytro.lukashenko@swarco.de>
2024-04-16 12:36:53 +0300

    add solutions for the reverse_string

---
+++ reverse_string/index.js
@@ -0,0 +1,44 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// First solution
+// function reverse(str) {
+// return str.split('').reverse().join('')
+// }
+
+// Second solution
+// function reverse(str) {
+//   const reversedArr = [];
+//   const arr = str.split('');
+//   let reversedStr = '';
+//   for (let i = arr.length; i >= 0; i--) {
+//     reversedArr.push(arr[i]);
+//   }
+//   reversedStr = reversedArr.join('');
+//   return reversedStr;
+// }
+
+// Third solution
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (const character of str) {
+//     reversed = character + reversed;
+//   }
+//   console.log(reversed)
+//   return reversed;
+// }
+
+// Fourth solution
+function reverse(str) {
+  return str.split('').reduce((rev, char) => {
+    return char + rev
+  }, '')
+}
+
+module.exports = reverse;
